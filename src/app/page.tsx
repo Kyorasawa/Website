00f69f2816548a103dab4867c297be3dc6ded57d
Kyora <kyora.uwu@proton.me>
2025-05-20 17:05:47 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,27 @@ export default function Home() {
             </div>
 
             <div>
-                <img src="http://github.com/kyorasawa.png" alt="GitHub Avatar" />
+                <ul className="profile">
+                    <li>
+                        <img
+                            src="http://github.com/kyorasawa.png"
+                            alt="GitHub Avatar"
+                            className="rounded-img"
+                        />
+                    </li>
+                    <li>
+                        <strong>#Kyora(Kyorasawa)</strong><br/>
+                        🔧 Passionate about full-stack web development and open source.<br/>
+                        🌱 Growing every day — learning, experimenting, evolving.<br/>
+                        💡 I value clean, readable code and simple yet useful ideas.<br/>
+                        🐧 A Linux enthusiast diving deep into the world of free software.
+                    </li>
+                </ul>
+                <div className="socials">
+                    <br/>
+                    <h3 className="about">Socials</h3>
+                    <Link href="https://github.com/Kyorasawa" className="tui-link2">./github</Link>
+                </div>
             </div>
         </div>
     );
